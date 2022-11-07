6a062902b603ff99451d8fe3816f545f2f60ecf3
Lulky <codingblocks@Codings-MacBook-Pro.local>
2022-11-07 11:46:51 +0530

    Patterns

---
+++ 06-11/pattern_triangle_number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i = 1, j = 1, x = 1;
+	int n;
+	cin>>n;
+	while(i<=n){
+		j = 1;
+		while(j <= i){
+			cout<<x<<" ";
+			x = x+1;
+			j = j+1;
+		}
+		cout<<endl;
+		i = i+1;
+	}
+	return 0;
+}
